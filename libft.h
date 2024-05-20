20e0e563a03ef6af58129dcf88e048d6fbd9c96f
Kelian Pouliquen <kepouliq@bess-f2r1s16.clusters.42paris.fr>
2024-05-20 18:09:36 +0200

    test

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kepouliq <kepouliq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 15:38:17 by kepouliq          #+#    #+#             */
+/*   Updated: 2024/05/20 16:19:46 by kepouliq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+
+#endif
