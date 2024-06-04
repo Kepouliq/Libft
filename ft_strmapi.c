8b4e504e8e09056cff12415c7d5a1d94debbbdab
Kelian Pouliquen <kepouliq@made-f0Ar7s9.clusters.42paris.fr>
2024-06-04 11:36:58 +0200

    more

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,46 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_striterie.c                                     :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kepouliq <kepouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/01 18:36:49 by kepouliq          #+#    #+#             */
-/*   Updated: 2024/06/03 12:53:20 by kepouliq         ###   ########.fr       */
+/*   Created: 2024/06/03 12:55:57 by kepouliq          #+#    #+#             */
+/*   Updated: 2024/06/03 13:08:21 by kepouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char			*tab;
 	unsigned int	i;
 
 	if (!s || !f)
-		return ;
+		return (NULL);
 	i = 0;
+	tab = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!tab)
+		return (NULL);
 	while (s[i])
 	{
-		(*f)(i, &s[i]);
+		tab[i] = f(i, s[i]);
 		i++;
 	}
+	tab[i] = '\0';
+	return (tab);
 }
-
-/*
-#include <stdio.h>
-
-void print_index_and_char(unsigned int i, char* c)
-{
-	if (*c >= 97 && *c <= 122)
-		*c -= 32;
-	else if (*c >= 65 && *c <= 90)
-		*c += 32;
-	printf("Index %d: %c\n", i, *c);
-}
-
-int main()
-{
-	char str[] = "Salut TEST un DeuX!";
-	ft_striteri(str, print_index_and_char);
-	return 0;
-}*/
